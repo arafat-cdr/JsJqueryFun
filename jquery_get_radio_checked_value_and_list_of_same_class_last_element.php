2b530e52e257ee9eeeb1da7189c21df538d1c166
Arafat <arafat.dml@gmail.com>
2023-02-18 02:23:25 +0600

    Create jquery_get_radio_checked_value_and_list_of_same_class_last_element.php
    Jquery Trick

---
+++ jquery_get_radio_checked_value_and_list_of_same_class_last_element.php
@@ -0,0 +1,3 @@
+var wl_check = jQuery('.account-type-radio:checked').val();
+
+jQuery('.dz-preview:last').after('<div class="wl_add_plus" style="display: inline-block"><span><i class="sl sl-icon-plus" style="color: red;font-size: 100px;float: right;padding-top: 35px;pointer-events: all;cursor: pointer;"></i></span></div>');
